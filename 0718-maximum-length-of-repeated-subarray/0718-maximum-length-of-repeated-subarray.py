3fbd112da7c07220ab84a4ed7da988d8a058330f
Chuan Yin <chuanyinyinchuan@gmail.com>
2024-03-26 13:41:03 -0500

    Time: 1464 ms (43.95%), Space: 41.4 MB (74.30%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -1,22 +1,35 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         # dp: {position: count}
-        dp = defaultdict(int)
-        max_val = 0
-        for n1 in nums1:
-            new_dp = defaultdict(int)
-            inds = [index for index, value in enumerate(nums2) if value == n1]
-            # print(inds)
-            if not inds:
-                dp = defaultdict(int)
-            else:
-                for ind in inds:
-                    if (ind - 1) in dp:
-                        new_dp[ind] = dp[ind-1] + 1
-                    else:
-                        new_dp[ind] = 1
-                dp = new_dp
-                # print(dp)
-                max_val = max(max_val, max(dp.values()))
+#         dp = defaultdict(int)
+#         max_val = 0
+#         for n1 in nums1:
+#             new_dp = defaultdict(int)
+#             inds = [index for index, value in enumerate(nums2) if value == n1]
+#             # print(inds)
+#             if not inds:
+#                 dp = defaultdict(int)
+#             else:
+#                 for ind in inds:
+#                     if (ind - 1) in dp:
+#                         new_dp[ind] = dp[ind-1] + 1
+#                     else:
+#                         new_dp[ind] = 1
+#                 dp = new_dp
+#                 # print(dp)
+#                 max_val = max(max_val, max(dp.values()))
         
-        return max_val
+#         return max_val
+    
+#         ChatGPT
+        m, n = len(nums1), len(nums2)
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        max_length = 0
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if nums1[i-1] == nums2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                    max_length = max(max_length, dp[i][j])
+                    
+        return max_length
